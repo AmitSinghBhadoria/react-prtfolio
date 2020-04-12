487b88485a0965582717819529c871cfb02e6c2c
Amit Bhadoria <amit.bhadoria@varahitechnologies.com>
2020-04-13 02:39:09 +0530

    Updates

---
+++ precache-manifest.882f607be2cb87d5ce501a4743ca61b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac0f36b4b5d0464f7b68e6f10cb5628",
+    "url": "/react-prtfolio/index.html"
+  },
+  {
+    "revision": "05aa77035d7cdad1469f",
+    "url": "/react-prtfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fac6c3fe93a3f28a7b8",
+    "url": "/react-prtfolio/static/js/2.21553678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-prtfolio/static/js/2.21553678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05aa77035d7cdad1469f",
+    "url": "/react-prtfolio/static/js/main.220ec28f.chunk.js"
+  },
+  {
+    "revision": "b9e743b3df7f00c0f191",
+    "url": "/react-prtfolio/static/js/runtime-main.14ec2da9.js"
+  }
+]);
